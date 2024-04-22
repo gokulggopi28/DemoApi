1f986648d03d6d21bc2be5b913b6e746a0629c5c
Gokul G Gopi <49408569+gokulggopi28@users.noreply.github.com>
2024-04-22 22:58:50 +0530

    Initial Commit

---
+++ test_api/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  test_api
+//
+//  Created by Gokul G Gopi on 22/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var user: GitHubUser?
+    var body: some View {
+        VStack(spacing: 20){
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                
+            }placeholder: {
+            Circle()
+                .foregroundColor(.secondary)
+        }
+                .frame(width: 120, height: 120)
+            
+            Text(user?.login ?? "Login Placeholder")
+                .bold()
+                .font(.title3)
+            Text(user?.bio ?? "Bio Placeholder")
+                .padding()
+            
+            Spacer()
+        }
+        .padding()
+        .task {
+            do{
+                user = try await getUser()
+            }catch GHError.invalidURL{
+                print("Invalid URL")
+                
+            }catch GHError.invalidResponse{
+                print("Invalid Response")
+            }catch GHError.invalidData{
+                print("Invalid Data")
+            }catch{
+                print("Unexpected error")
+            }
+        }
+    }
+    func getUser() async throws -> GitHubUser{
+        let endpoint = "https://api.github.com/users/gokulggopi28"
+        guard let url  = URL(string: endpoint) else{
+            throw GHError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw GHError.invalidResponse
+        }
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitHubUser.self, from: data)
+        }
+        catch{
+            throw GHError.invalidData
+        }
+    }
+}
+
+struct ContentView_Preview: PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
+}
+struct GitHubUser: Codable{
+    let login: String
+    let avatarUrl: String
+    let bio: String
+    
+    
+}
+
+enum GHError: Error{
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
---
+++ test_api/test_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_apiApp.swift
+//  test_api
+//
+//  Created by Gokul G Gopi on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct test_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
